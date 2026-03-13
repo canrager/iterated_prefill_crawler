973212df469fd87af4ec0dfb0598abae5ebde26a
Avery Yen <976748+haplesshero13@users.noreply.github.com>
2026-03-13 18:46:53 +0000

    Clean everything up and combine templates

--- test_semaphore.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-async def test():
-    sem = asyncio.Semaphore(10)
-    async def run(i):
-        async with sem:
-            return i
-    tasks = [run(i) for i in range(20)]
-    return await asyncio.gather(*tasks)
-print(asyncio.run(test()))
